2f3dd52827259d1b8cc99d674ae2dd201b9ea094
Filip Fog <hanfil@outlook.com>
2025-06-05 08:28:43 +0200

    troubleshooting

--- internal-export-file/export-report-pdf-sicra/src/export-report-pdf.py
+++ internal-export-file/export-report-pdf-sicra/src/export-report-pdf.py
@@ -764,7 +764,17 @@ class ExportReportPdf:
         case_severity = case_dict["severity"]
         case_tasks = case_dict["tasks"]
         response_types = case_dict["response_types"]
-        case_priority_desc = self.helper.api_impersonate.vocabulary.read(name=case_priority)
+        case_priority_filters = {
+            "mode": "and",
+            "filters": [{
+                "key": ["name"],
+                "values": [case_priority],
+                "operator": "eq",
+                "mode": "or"
+            }],
+            "filterGroups": []
+        }
+        case_priority_desc = self.helper.api_impersonate.vocabulary.read(filters=case_priority_filters)
         # Store context for usage in html template
         context = {
             "case_name": case_name,
